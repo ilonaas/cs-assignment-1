21546ea33445e4368430a91c6ff0e87f37dd4d7b
Ilona Shabou <ilona_shabou@hotmail.com>
2022-10-06 14:44:19 +0200

    klar

--- AddressBook/MenuManager.cs
+++ AddressBook/MenuManager.cs
@@ -23,6 +23,7 @@ namespace AddressBook
 
             return Console.ReadLine();
         }
+        // här visas 4 alternativ som användaren får välja emellan.
 
         public void ViewContactsMenu()
         {
@@ -34,73 +35,77 @@ namespace AddressBook
                 Console.WriteLine($"{contact.Id} - {contact.FirstName} {contact.LastName}, {contact.StreetName} {contact.PostalCode} {contact.City}");
             }
         }
-
-        // 
+        // den här metoden visar användaren alla kontakter som finns i adressboken.
+         
         public void CreateNewContactMenu()
         {
             Console.Clear();
             Console.WriteLine("*** CREATE CONTACT ***");
 
             var contact = new Contact();
+            contact.Id = Guid.NewGuid();
 
             Console.Write("Firstname: ");
-            var firstName = Console.ReadLine();
+            contact.FirstName = Console.ReadLine();
 
             Console.Write("Lastname: ");
-            var lastName = Console.ReadLine();
+            contact.LastName = Console.ReadLine();
 
             Console.Write("Streetname: ");
-            var StreetName = Console.ReadLine();
+            contact.StreetName = Console.ReadLine();
 
             Console.Write("Postalcode: ");
-            var PostalCode = Console.ReadLine();
+            contact.PostalCode = Console.ReadLine();
 
             Console.Write("City: ");
-            var City = Console.ReadLine();
+            contact.City = Console.ReadLine();
 
             contacts.Add(contact);
         }
+        // här kan användaren skapa en ny kontakt.
 
         public void ViewOneContactMenu()
         {
-            Console.Clear();
-            Console.WriteLine("*** CONTACT DETAILS ***"); 
-            Console.Write("Enter contact Id to view: ");
-            var id = Guid.Parse(Console.ReadLine());
-
-            var contact = contacts.FirstOrDefault(x => x.Id == id);
-
-            Console.WriteLine($"Firstname: {contact.FirstName}");
-            Console.WriteLine($"Lastname: {contact.LastName}");
-            Console.WriteLine($"Streetname: {contact.StreetName}");
-            Console.WriteLine($"Postalcode: {contact.PostalCode}");
-            Console.WriteLine($"City: {contact.City}");
-            Console.WriteLine();
-
-            Console.WriteLine("1. Edit contact.");
-            Console.WriteLine("2. Delete contact.");
-            Console.Write("Enter option: ");
-            var option = Console.ReadLine();
-            // här har jag skrivit två alternativ som användaren kan välja mellan
-
-            switch (option)
+            try
             {
-                case "1":
-                    UpdateContact(contact);
-                    break;
+                Console.Clear();
+                Console.WriteLine("*** CONTACT DETAILS ***");
+                Console.Write("Enter contact Id to view: ");
+                var id = Guid.Parse(Console.ReadLine());
 
-                case "2":
-                    DeleteContact(contact.Id);
-                    break;
+                var contact = contacts.FirstOrDefault(x => x.Id == id);
+                
+                Console.WriteLine();
+                Console.WriteLine($"Firstname: {contact.FirstName}");
+                Console.WriteLine($"Lastname: {contact.LastName}");
+                Console.WriteLine($"Streetname: {contact.StreetName}");
+                Console.WriteLine($"Postalcode: {contact.PostalCode}");
+                Console.WriteLine($"City: {contact.City}");
+                Console.WriteLine();
 
-                case "3":
-                    OptionsMenu();
-                    break;
+                Console.WriteLine("1. Edit contact.");
+                Console.WriteLine("2. Delete contact.");
+                Console.Write("Enter option: ");
+                var option = Console.ReadLine();
+                // här har jag skrivit två alternativ som användaren kan välja mellan
 
-                default:
-                    break;
+                switch (option)
+                {
+                    case "1":
+                        UpdateContact(contact);
+                        break;
+
+                    case "2":
+                        DeleteContact(contact.Id);
+                        break;
+
+                    default:
+                        break;
+                }
             }
+            catch { }
         }
+        // den här metoden visar användaren en specifik kontakt från adressboken.
 
         public void UpdateContact(Contact contact)
         {
@@ -134,6 +139,7 @@ namespace AddressBook
                 contact.City = city;
             // här kommer kontakten uppdateras och sparas
         }
+        // om anvädaren väljer detta alternativ så kan en kontakt uppdateras.
 
         public void DeleteContact(Guid id)
         {
@@ -141,6 +147,7 @@ namespace AddressBook
             var option = Console.ReadLine();
             contacts = contacts.Where(x => x.Id != id).ToList();
         }
+        //här kan användaren välja en kontakt som kan raderas.
 
         public void Run()
         {
@@ -148,7 +155,7 @@ namespace AddressBook
             {
                 Console.Clear();
                 var option = OptionsMenu();
-                switch (option)
+                switch (option.ToUpper())
                 {
                     case "1":
                         ViewContactsMenu();
@@ -173,7 +180,10 @@ namespace AddressBook
             } while (true);
 
         }
+        // de 3 alternativen från huvudmenyn körs genom denna.
 
     }
+     
+
 
 }
